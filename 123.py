0219e928992ff67ec1ee735ddb7c211eee6ec25e
FaustSmile <s56142@yahoo.com.tw>
2022-04-03 18:59:11 +0800

    First-Commit

---
+++ 123.py
@@ -0,0 +1,11 @@
+# data types (種類)
+# 資料型別
+# 1. 整數 integer int
+# 2. 浮點數(小數) float
+# 3. 布林值 boolean bool   (True, False)
+# 4. 字串 string str  (文字)
+price = 10
+pen = 0.38
+rain =True
+name = "Faust"
+print(price, pen, rain, name)
